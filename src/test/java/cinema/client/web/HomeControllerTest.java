a40b17820e1b33c66bc54f8b1a6608d46963b68f
moiseevg <grisha9319>
2016-04-12 20:44:22 +0300

    Добавлено: исправил тесты

--- src/test/java/cinema/client/web/HomeControllerTest.java
+++ src/test/java/cinema/client/web/HomeControllerTest.java
@@ -1,23 +1,48 @@
 package cinema.client.web;
 
+import cinema.client.SetupData.SessionSetupData;
+import cinema.client.entity.Film;
+import cinema.client.entity.Session;
+import cinema.client.service.FilmService;
+import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 
 public class HomeControllerTest {
 
-    HomeController controller;
-    MockMvc mockMvc;
+    @Test
+    public void shouldShowRecentFilms() throws Exception {
+        List<Film> expectedFilms = SessionSetupData.setupData()
+                .stream()
+                .map(Session::getFilm)
+                .collect(Collectors.toList());
+        FilmService mockService =
+                mock(FilmService.class);
+        when(mockService.findAll())
+                .thenReturn(expectedFilms);
+        HomeController controller =
+                new HomeController(mockService);
+        MockMvc mockMvc = standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/home.jsp"))
+                .build();
+        mockMvc.perform(get("/"))
+                .andExpect(view().name("home"))
+                .andExpect(model().attributeExists("filmList"))
+                .andExpect(model().attribute("filmList",
+                        hasItems(expectedFilms.toArray())));
+    }
 
-//    @Before
-//    public void setup() throws Exception {
-//        controller = new HomeController();
-//        mockMvc = standaloneSetup(controller).build();
-//    }
-//
-//    @Test
-//    public void testHomePage() throws Exception {
-//        mockMvc.perform(get("/"))
-//                .andExpect(view().name("home"));
-//    }
 
 }
